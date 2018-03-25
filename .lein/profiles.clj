0b1bc744496a1630e65354918f4bf79aeddb4b6b
Jeff Wong <awole20@gmail.com>
2018-03-25 12:23:33 -0700

    update lein profiles

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,10 +1,3 @@
 {:user
   {
-   :dependencies [[org.clojure/tools.nrepl "0.2.12"]]
-   :plugins [[cider/cider-nrepl "0.15.0-SNAPSHOT"]
-             [lein-ancient "0.6.10"]]}
-
- :repl
- {
-  :dependencies [[org.clojure/tools.nrepl "0.2.12"]]
-  :plugins [[cider/cider-nrepl "0.15.0-SNAPSHOT"]]}}
+   :plugins [[lein-ancient "0.6.10"]]}}
